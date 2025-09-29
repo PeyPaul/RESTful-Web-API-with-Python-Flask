9425ea25ae7fd83ad73c4c9f3433c44b86f99407
Paul PEYTEVIN <paulpeytevin@Pauls-MacBook-Air.local>
2025-09-29 15:20:11 +0200

    add the posibility to post binary message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, json
 
 app = Flask(__name__)
 
@@ -33,6 +33,24 @@ def api_echo():
         return "ECHO: PUT\n"
     elif request.method == 'DELETE':
         return "ECHO: DELETE\n"
+
+@app.route('/messages', methods = ['POST'])
+def api_message():
+    if request.headers['Content-Type'] == 'text/plain':
+        return "Text Message: " + request.data
+    
+    elif request.headers['Content-Type'] == 'application/json':
+        return "JSON Message: " + json.dumps(request.json)
+    
+    elif request.headers['Content-Type'] == 'application/octet-stream':
+        f = open('./binary', 'wb')
+        f.write(request.data)
+        f.close()
+        return "Binary message written!"
+    
+    else:
+        return "415 Unsupported Media Type"
+
 
 if __name__ == '__main__':
     app.run()
