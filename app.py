93c001f9f5939a52b13e96acf2a52763f1841171
Paul PEYTEVIN <paulpeytevin@Pauls-MacBook-Air.local>
2025-09-29 09:29:32 +0200

    just implemented request methods with http verbs

--- app.py
+++ app.py
@@ -21,5 +21,18 @@ def api_hello():
     else:
         return 'Hello unknown traveler'
 
+@app.route('/echo', methods = ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
+def api_echo():
+    if request.method == 'GET':
+        return "ECHO: GET\n"
+    elif request.method == 'POST':
+        return "ECHO: POST\n"
+    elif request.method == 'PATCH':
+        return "ECHO: PATCH\n"
+    elif request.method == 'PUT':
+        return "ECHO: PUT\n"
+    elif request.method == 'DELETE':
+        return "ECHO: DELETE\n"
+
 if __name__ == '__main__':
     app.run()
